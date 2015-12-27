eaea99859f673fcb77c1e9ec0cc6dfe25733e6d6
Valérian Girard <valerian.girard@gmail.com>
2015-12-27 10:51:59 +0100

    Add a page for multiple

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -37,6 +37,53 @@ class DefaultController extends Controller
                             "Price"         => 'p.price',
                             "_identifier_"  => 'p.id')
                         )
+                ->setSearch(true)
+                ;
+    }
+    
+    
+    /**
+     * @Route("/multiple", name="multiple")
+     */
+    public function multipleAction()
+    {
+        $this->datatableMultipleProduct();
+        
+        return $this->render('default/multiple.html.twig', array());
+    }
+    
+    /**
+     * @Route("/multiple/list", name="multiple_product_list")
+     */
+    public function multipleProductListAction()
+    {
+        return $this->datatableMultipleProduct()->execute();
+    }
+    
+    private function datatableMultipleProduct()
+    {
+        return $this->get('datatable')
+                ->setEntity("AppBundle:Product", "p")
+                ->setFields(
+                        array(
+                            "Name"          => 'p.name',
+                            "Description"       => 'p.description',
+                            "Price"         => 'p.price',
+                            "_identifier_"  => 'p.id')
+                        )
+                ->setSearch(true)
+                ->setMultiple(
+                                array(
+                                    'delete' => array(
+                                        'title' => 'Delete',
+                                        'route' => 'multiple'
+                                    ),
+                                    'move' => array(
+                                        'title' => 'Move',
+                                        'route' => 'multiple'
+                                    ),
+                                )
+                        )
                 ;
     }
 }
